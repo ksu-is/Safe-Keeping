ad5a46ffc39b56cee98a9c7cf6e0f1de6e8cdfac
NickBahr <142357825+NickBahr@users.noreply.github.com>
2023-11-26 17:57:57 -0500

    Update safe-keeping.py
    import code

--- safe-keeping.py
+++ safe-keeping.py
@@ -1,1 +1,23 @@
+import os, errno
 
+
+# Function to write a file at a given path
+def WriteFileInPath(content, path):
+    try:
+        with open(path, "w+") as f:
+            f.write(content)  # TODO: CHECK IF NEW LINE CHARACTER IS NEEDED OR NOT
+            f.close()
+            return "Created " + path
+    except IOError as e:
+        print(e)
+        return False
+
+
+# This function creates given folders.
+def installFolder(folder):
+    try:
+        os.makedirs(folder)
+        print("Created directory", folder)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
